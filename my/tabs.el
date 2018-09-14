2bb135ddb4216f50ac48ac43d89f899b327b84f3
Evan Nagle <evan@mantle.co>
2018-09-14 12:46:29 -1000

    tabs as spaces, my/tabs lib

---
+++ my/tabs.el
@@ -0,0 +1,32 @@
+;;; tabs.el --- Configuration for the tabs package
+
+;; Copyright (C) 2018 Evan Nagle
+
+;; Author: Evan Nagle <evan@mantle.co>
+;; URL: https://github.com/evannagle/my-emacs
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(defvar my/tabs/are-spaces t)
+
+(if my/tabs/are-spaces
+    (progn
+      (setq tab-width 4) 
+      (setq indent-tabs-mode t))
+  (setq-default indent-tabs-mode nil))
+
+(provide 'my/tabs)
+
+;;; tabs.el ends here
