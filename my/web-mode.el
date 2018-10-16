1bcbb6d7c7abd61a5630b5b81920d2bfb71f702a
Evan Nagle <evan@mantle.co>
2018-10-16 10:11:59 -1000

    Remove twig from web-mode

--- my/web-mode.el
+++ my/web-mode.el
@@ -28,8 +28,7 @@
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
 
 (provide 'my/web-mode)
 
